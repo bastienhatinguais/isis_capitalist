57cf2232c522900e034f19d9531658a6fa85216e
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2023-06-01 10:42:53 +0200

    Pas mal le code la :)

---
+++ front/src/app/modal/manager/manager.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Palier } from '../../model/palier';
+import { World } from '../../model/world';
+import { WebserviceService } from '../../webservice.service';
+
+@Component({
+  selector: 'app-modal-manager',
+  templateUrl: './manager.component.html',
+  styleUrls: ['./manager.component.css'],
+})
+export class ManagerComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { world: World; server: string },
+    private wsService: WebserviceService
+  ) {}
+
+  hireManager(manager: Palier) {
+    if (this.data.world.money >= manager.seuil) {
+      this.data.world.money -= manager.seuil;
+      manager.unlocked = true;
+      this.data.world.products[manager.idcible - 1].managerUnlocked = true;
+      this.wsService
+        .engagerManager(manager.name)
+        .catch((error) => console.log(error));
+    }
+  }
+}
